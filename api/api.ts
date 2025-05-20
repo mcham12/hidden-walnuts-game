a3a17060d20bbbc57d905a23805ab42790d3e198
mcham12 <mattmcarroll@me.com>
2025-05-20 13:25:26 -0500

    latest mvp 4.5 debugging

--- api/api.ts
+++ api/api.ts
@@ -8,4 +8,22 @@ app.post('/rehide-test', async (c) => {
   const response = await forestManager.fetch('http://internal/rehide-test');
   const result = await response.json();
   return c.json(result);
+});
+
+app.get('/map-state', async (c) => {
+  const id = c.env.FOREST_MANAGER.idFromName('forest');
+  console.log('DO ID for /map-state:', id.toString());
+  const forestManager = c.env.FOREST_MANAGER.get(id);
+  const response = await forestManager.fetch('http://internal/map-state');
+  const result = await response.json();
+  return c.json(result);
+});
+
+app.get('/join', async (c) => {
+  const id = c.env.FOREST_MANAGER.idFromName('forest');
+  console.log('DO ID for /join:', id.toString());
+  const forestManager = c.env.FOREST_MANAGER.get(id);
+  const response = await forestManager.fetch('http://internal/join');
+  const result = await response.json();
+  return c.json(result);
 }); 
