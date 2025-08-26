e08f6dc8fa9142421550d395423aeefb7adee813
mcham12 <mattmcarroll@me.com>
2025-08-26 15:44:19 -0500

    fix build errors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,26 +1,27 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+
 export default defineConfig({
-base: '/',
-resolve: {
-alias: {
-'@': path.resolve(__dirname, './src'),
-},
-},
-server: {
-port: 5173,
-open: true,
-},
-build: {
-outDir: 'dist',
-sourcemap: true,
-rollupOptions: {
-output: {
-manualChunks: {
-three: ['three'],
-},
-},
-},
-},
-assetsInclude: ['**/*.glb'],
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+        },
+      },
+    },
+  },
+  assetsInclude: ['**/*.glb'],
 });
